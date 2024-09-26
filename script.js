f5986df62bd067cd828d046e7458d3bce9610b20
Mayfryn <33200716+Mayfryn@users.noreply.github.com>
2024-09-26 15:25:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showTab(tabId) {
+    const contents = document.querySelectorAll('.tab-content');
+    contents.forEach(content => content.style.display = 'none');
+    document.getElementById(tabId).style.display = 'block';
+}
